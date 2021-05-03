f0cdd30fc399ceb7c23e67539cc0e0763d5e9b44
Nithya <nithyaasworld@gmail.com>
2021-05-03 15:25:18 +0530

    with history

--- src/components/Board/Board.jsx
+++ src/components/Board/Board.jsx
@@ -5,7 +5,16 @@ export default function Board(props) {
   let [squares, setSquares] = useState(new Array(9).fill(" "));
   let [currentPlayer, setCurrentPlayer] = useState("X");
   let [gameState, setGameState] = useState("In Progress");
+  let [history, setHistory] = useState([squares]);
+  let [currHistoryItem, setCurrHistoryItem] = useState(1);
+
   let clickHandler = (index) => {
+    console.log({ currHistoryItem });
+    console.log({ history });
+    let historyCopy = JSON.parse(JSON.stringify(history));
+    historyCopy = historyCopy.slice(0, currHistoryItem + 1);
+    setHistory(historyCopy);
+    
     if (["X", "O"].includes(squares[index])) {
       return;
     }
@@ -15,10 +24,23 @@ export default function Board(props) {
     let squaresCopy = [...squares];
     squaresCopy[index] = currentPlayer;
     setSquares(squaresCopy);
+
+    let historyCopy2 = JSON.parse(JSON.stringify(historyCopy));
+    historyCopy2.push(squaresCopy);
+    setHistory(historyCopy2);
+  
     if (currentPlayer === "X") setCurrentPlayer("O");
     else setCurrentPlayer("X");
     checkWinner(squaresCopy);
+    setCurrHistoryItem(currHistoryItem + 1);
   };
+
+  let generateBoardFromHistory = (history, i) => {
+    let currHistoryCopy = [...history[i]];
+    setSquares(currHistoryCopy);
+    setCurrHistoryItem(i);
+  }
+
   let checkWinner = (squaresCopy) => {
     let firstRow = squaresCopy.slice(0, 3).join("");
     if (isThereAWinner(firstRow)) return;
@@ -56,7 +78,10 @@ export default function Board(props) {
         <p>Game State: {gameState}</p>
       </div>
       <div className="board" style={{ pointerEvents: gameState !== 'In Progress' ? 'none' : 'auto' }}>
-        {squares.map((cell, i) => <Square value={cell} index={i} clickHandler={clickHandler} gameState={gameState} />)}
+        {squares.map((cell, i) => <Square key={i} value={cell} index={i} clickHandler={clickHandler} gameState={gameState} />)}
+      </div>
+      <div className="history" >
+        {history.map((item, i) => <button onClick={() => generateBoardFromHistory(history, i)} style={{width: '100px'}} key={i}>Go to Step: {i}</button>)}
       </div>
     </>
   );
--- src/components/Square/Square.jsx
+++ src/components/Square/Square.jsx
@@ -1,6 +1,16 @@
-import './Square.css';
+import "./Square.css";
 export default function Square(props) {
-    return (
-        <button style={{backgroundColor: props.gameState!=='In Progress' ? 'rgb(221, 218, 218, 0.5)' : 'white'}} onClick={(e)=>props.clickHandler(props.index)}>{props.value}</button>
-    )
+  return (
+    <button
+      style={{
+        backgroundColor:
+          props.gameState !== "In Progress"
+            ? "rgb(221, 218, 218, 0.5)"
+            : "white",
+      }}
+      onClick={() => props.clickHandler(props.index)}
+    >
+      {props.value}
+    </button>
+  );
 }
